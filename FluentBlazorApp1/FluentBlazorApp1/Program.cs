32ee0d3cb5346b5a9b63b577353f47d0c2acf82f
Seany <17074267@qq.com>
2024-04-13 22:46:54 +0800

    添加项目文件。

---
+++ FluentBlazorApp1/FluentBlazorApp1/Program.cs
@@ -0,0 +1,34 @@
+using FluentBlazorApp1.Client.Pages;
+using FluentBlazorApp1.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+	.AddInteractiveServerComponents()
+	.AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddFluentUIComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseWebAssemblyDebugging();
+}
+else
+{
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+	.AddInteractiveServerRenderMode()
+	.AddInteractiveWebAssemblyRenderMode()
+	.AddAdditionalAssemblies(typeof(FluentBlazorApp1.Client._Imports).Assembly);
+
+app.Run();
